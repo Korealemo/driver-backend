87e34d4808f70f91e5b1d57bddd81fbc547a21db
Lemohang <lemohangmokhobo@gmail.com>
2026-05-07 22:54:01 +0200

    fix mysql env config

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 8080;
 console.log("🔎 DB ENV CHECK:", {
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
-  database: process.env.MYSQL_DATABASE,
+  database: process.env.MYSQLgitDATABASE,
   port: process.env.MYSQLPORT,
 });
 
